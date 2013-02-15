aaf5a2bd82d579ef1043b7f05f3397a6f6265976
Stephane Charette <stephanecharette@gmail.com>
2013-02-14 22:24:53 -0800

    first version to build; only OID included

--- code/src/SNMPpp.cpp
+++ code/src/SNMPpp.cpp
@@ -2,5 +2,5 @@
 // SNMPpp project uses the MIT license. See LICENSE for details.
 // Copyright (C) 2013 Stephane Charette <stephanecharette@gmail.com>
 
-#include <SNMPpp.hpp>
+#include <SNMPpp/SNMPpp.hpp>
 
